7c01ead65ddfcb9b3f431e35bd733cbd7d9ae900
dvir <dvir019@gmail.com>
2019-05-25 20:12:54 +0300

    Add clipboard functions to the toolstrip

--- FinalProjectWinForms/FinalProjectWinForms/Form1.Designer.cs
+++ FinalProjectWinForms/FinalProjectWinForms/Form1.Designer.cs
@@ -54,7 +54,7 @@
             this.tsSelectFontColor = new System.Windows.Forms.ToolStripMenuItem();
             this.tsBackgroundColor = new System.Windows.Forms.ToolStripDropDownButton();
             this.tsSelectBackgroundColor = new System.Windows.Forms.ToolStripMenuItem();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rtb = new System.Windows.Forms.RichTextBox();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -302,21 +302,21 @@
             this.tsSelectBackgroundColor.Size = new System.Drawing.Size(180, 22);
             this.tsSelectBackgroundColor.Text = "Select color...";
             // 
-            // richTextBox1
+            // rtb
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(48, 54);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(728, 384);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.WordWrap = false;
+            this.rtb.Location = new System.Drawing.Point(48, 54);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(728, 384);
+            this.rtb.TabIndex = 2;
+            this.rtb.Text = "";
+            this.rtb.WordWrap = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.rtb);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -358,7 +358,7 @@
         private System.Windows.Forms.ToolStripMenuItem tsSelectFontColor;
         private System.Windows.Forms.ToolStripDropDownButton tsBackgroundColor;
         private System.Windows.Forms.ToolStripMenuItem tsSelectBackgroundColor;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rtb;
     }
 }
 
--- FinalProjectWinForms/FinalProjectWinForms/ToolStripFunctions.cs
+++ FinalProjectWinForms/FinalProjectWinForms/ToolStripFunctions.cs
@@ -12,6 +12,39 @@ namespace FinalProjectWinForms
 {
     public partial class Form1 : Form
     {
-        
+
+        #region Clipboard management
+
+        /// <summary>
+        /// Handles the Click event of the tsCut control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void tsCut_Click(object sender, EventArgs e)
+        {
+            rtb.Cut();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the tsCopy control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void tsCopy_Click(object sender, EventArgs e)
+        {
+            rtb.Copy();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the tsPaste control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void tsPaste_Click(object sender, EventArgs e)
+        {
+            rtb.Paste();
+        }
+
+        #endregion Clipboard management
     }
 }

1a273a0bd07012f3b5bfd9f549fd9a5d448a2629
dvir <dvir019@gmail.com>
2019-05-26 19:09:44 +0300

    Fix Undo and Redo functions

--- FinalProjectWinForms/FinalProjectWinForms/ToolStripFunctions.cs
+++ FinalProjectWinForms/FinalProjectWinForms/ToolStripFunctions.cs
@@ -58,7 +58,7 @@ namespace FinalProjectWinForms
         {
             if (rtb.CanUndo)
                 rtb.Undo();
-            else
+            if (!rtb.CanUndo)
                 toolStripUndo.Enabled = false;
         }
 

1d33614672fa38da0b7d126eb55aa185bae0f73b
dvir <dvir019@gmail.com>
2019-06-22 20:34:07 +0300

    Create editsForm form with function to add new line

---
+++ FinalProjectWinForms/FinalProjectWinForms/EditsForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FinalProjectWinForms
+{
+    partial class EditsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.editsTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // editsTextBox
+            // 
+            this.editsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editsTextBox.Location = new System.Drawing.Point(0, 0);
+            this.editsTextBox.Name = "editsTextBox";
+            this.editsTextBox.ReadOnly = true;
+            this.editsTextBox.Size = new System.Drawing.Size(357, 266);
+            this.editsTextBox.TabIndex = 0;
+            this.editsTextBox.Text = "";
+            // 
+            // EditsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 266);
+            this.Controls.Add(this.editsTextBox);
+            this.Name = "EditsForm";
+            this.Text = "Edits";
+            this.Load += new System.EventHandler(this.EditsForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox editsTextBox;
+    }
+}
---
+++ FinalProjectWinForms/FinalProjectWinForms/EditsForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProjectWinForms
+{
+    public partial class EditsForm : Form
+    {
+        public EditsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EditsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Adds a new line to the textBox.
+        /// </summary>
+        /// <param name="text">The text of the new line</param>
+        public void AddNewLine(string text)
+        {
+            if (editsTextBox.Text.Trim() != "")
+                editsTextBox.AppendText("\n");
+            editsTextBox.AppendText(text);
+        }
+    }
+}
